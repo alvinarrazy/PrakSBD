d4260f9a80d1ac7e761ca9d272569544dfe9b12e
Alvin Arrazy <51030862+alvinarrazy@users.noreply.github.com>
2020-11-13 23:47:06 +0700

    Commit

---
+++ config.php
@@ -0,0 +1,10 @@
+<?php
+
+$databaseHost = 'localhost';
+$databaseName = 'modul1_sbd_db';
+$databaseUsername = 'root';
+$databasePassword = '';
+
+$mysqli = mysqli_connect($databaseHost, $databaseUsername, $databasePassword, $databaseName); 
+
+?>
